9f1ce866254556f6016afc941d4d4c96b6c27196
sametkzdl <sametkazdal_53@outlook.com>
2024-08-20 19:07:53 +0300

    düzenlemeler yapılar canlıya çıkıldı ama local patladı

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Fragment } from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import FormApply from "./pages/form";
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/FormApply" element={<FormApply />} />
-        <Route path={"*"} element={<Error />} />
-      </Routes>
+      <BrowserRouter basename="/CoachWebSite">
+        <Routes>
+          <Route path="/CoachWebSite" element={<Home />} />
+          <Route path="/FormApply" element={<FormApply />} />
+          <Route path={"*"} element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </Fragment>
   );
 }
 
 function Error() {
   return (
-    <div >
+    <div>
       <h2>Böyle bir sayfa bulunamadı</h2>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./utils/global";
 import { BlurProvider } from "./context/blurContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <BlurProvider>
         <App />
       </BlurProvider>
     </ThemeProvider>
-  </BrowserRouter>
 );
